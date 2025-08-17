885cfb4cd1f84391b8e45a36002f17faf17021ee
Omori.Q <fabianestrada716@gmail.com>
2025-08-17 14:04:15 -0500

    Instalación y configuración del proyecto

---
+++ src/main/java/idat/pe/security_config_ef/SecurityConfigEfApplication.java
@@ -0,0 +1,13 @@
+package idat.pe.security_config_ef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityConfigEfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityConfigEfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idat/pe/security_config_ef/SecurityConfigEfApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.pe.security_config_ef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityConfigEfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
